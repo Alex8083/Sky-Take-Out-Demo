9a904319c0ae606c99c7746f76df33c80ee63e06
Alex8083 <3321424572@qq.com>
2026-03-31 15:37:24 +0800

    添加了登录和新增员工模块

---
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.sky.service;
+
+import com.sky.dto.EmployeeDTO;
+import com.sky.dto.EmployeeLoginDTO;
+import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
+
+public interface EmployeeService {
+
+    /**
+     * 员工登录
+     * @param employeeLoginDTO
+     * @return
+     */
+    Employee login(EmployeeLoginDTO employeeLoginDTO);
+
+    /*
+    * 新增员工
+    *
+    * */
+    void addEmployee(EmployeeDTO employeeDTO);
+}
